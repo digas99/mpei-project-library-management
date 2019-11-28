cb1b0556c934d8f27f1bdbcaf50bc69908254640
Diogo <diogo.correia99@ua.pt>
2019-11-28 14:59:53 +0000

    added function to FetchFromWeb to retreive an array of titles and authors. Added a function to remove weird spacement of the titles and authors due to the HTML fortmation. The class test now displays 20 books from each website.

--- data_base/source/FetchFromWeb.java
+++ data_base/source/FetchFromWeb.java
@@ -1,12 +1,17 @@
 import java.net.*;
 import java.util.Scanner;
-import java.io.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class FetchFromWeb {
 
     private String htmlContent;
+    private String name;
 
-    public FetchFromWeb(String link) {
+    public FetchFromWeb(String link, String name) {
+        this.name = name;
         String content = null;  
         URLConnection connection = null;
         try {
@@ -25,15 +30,70 @@ public class FetchFromWeb {
         return htmlContent;
     }
 
-    // public getBooksName() {
+    public String getName() {
+        return name;
+    }
+
+    public Map<String, String> getBooksInfoMap() {
+        Map<String, String> info = new HashMap<String, String>();
+        String[] titles = getBooksTitle();
+        String[] authors = getBooksAuthor();
+        for (int i=0; i<titles.length; i++) {
+            info.put(titles[i], authors[i]);
+        }
+        return info;
+    }
+
+    public String[] getBooksTitle() {
+        List<String> titles = new ArrayList<String>();
+        String[] split1 = htmlContent.split("f-searchResult__title");
+        for (int i = 1; i < split1.length; i++) {
+            String[] split2 = split1[i].split(">");
+            titles.add(removeSpacesFromBeginningAndEndOfString(split2[1].split("<")[0], false));
+        }
+        return titles.toArray(new String[titles.size()]);
+    }
+
+    public String[] getBooksAuthor() {
+        List<String> authors = new ArrayList<String>();
+        String[] split1 = htmlContent.split("searchResult__subtitle");
+        for (int i = 1; i < split1.length; i++) {
+            String[] split2 = split1[i].split(">");
+            authors.add(removeSpacesFromBeginningAndEndOfString(split2[2].split("<")[0], false));
+        }
+        return authors.toArray(new String[authors.size()]);
+    }
+
+    private String removeSpacesFromBeginningAndEndOfString(String s, boolean stopReversing) {
+        String newS = "";
+        boolean beginningFlag = false;
+        for (int i=0; i<s.length(); i++) {
+            // System.out.println("BeggingFlag: "+beginningFlag);
+            char currentChar = s.charAt(i);
+            if (!beginningFlag) {
+                if (!Character.isWhitespace(currentChar)) {
+                    beginningFlag = true;
+                }
+            }
+            if (beginningFlag) {
+                newS += ""+currentChar;
+            }
+        }
+        // remove from end
+        if (!stopReversing) {
+            newS = removeSpacesFromBeginningAndEndOfString(reverse(newS), true);
+            // put back in correct order
+            return reverse(newS);
+        }
         
-    // }
+        return newS;
+    }
 
-    // public getBooksAuthor() {
-
-    // }
-
-    // public getBooksInfo() {
-
-    // }
+    private String reverse(String s) {
+        String newS = "";
+        for (int i=s.length()-1; i>=0; i--) {
+            newS += s.charAt(i);
+        }
+        return newS;
+    }
 }
--- data_base/source/test.java
+++ data_base/source/test.java
@@ -1,17 +1,25 @@
 import java.io.*;
+import java.util.Map;
 
 public class test {
+
+    public static FetchFromWeb[] ffwArray;
 
     public static void main(String[] args) throws IOException {
         String[] fileNames = {"livros_literatura", "livros_thriller", "livros_syfy", "livros_crianças"};
         String[] links = {"https://www.fnac.pt/n790158/Os-100-Melhores-Livros/Os-100-Melhores-Livros-de-Literatura", "https://www.fnac.pt/n790159/Os-100-Melhores-Livros/Os-100-Melhores-Livros-de-Policial-e-Thriller", "https://www.fnac.pt/n790160/Os-100-Melhores-Livros/Os-100-Melhores-Livros-de-Fantastico-e-Ficcao-Cientifica", "https://www.fnac.pt/n1284079/Os-100-Melhores-Livros/Biblioteca-Fnac-Kids/Livros-mais-de-10-anos"};
-        int loadingSize = 20;
+        ffwArray = new FetchFromWeb[fileNames.length];
+        int nmrFetched = 0;
+        int loadingSize = 50;
         int loadingDivision = loadingSize/fileNames.length;
+        System.out.println("Fetching pages...");
         System.out.print("Loading: [");
         for (int i=0; i<fileNames.length; i++) {
             File f = new File("html_pages/"+fileNames[i]+".html");
             PrintWriter pw = new PrintWriter(f);
-            FetchFromWeb ffw = new FetchFromWeb(links[i]);
+            FetchFromWeb ffw = new FetchFromWeb(links[i], fileNames[i]);
+            ffwArray[nmrFetched] = ffw;
+            nmrFetched++;
             String htmlContent = ffw.getHTML();
             for (int j=0; j<loadingDivision; j++) {
                 System.out.print("|");
@@ -20,6 +28,16 @@ public class test {
             pw.flush();
             pw.close();
         }
-        System.out.println("]");
+        System.out.println("]\n");
+
+        for (FetchFromWeb ffw : ffwArray) {
+            int counter=1;
+            System.out.println("-> "+ffw.getName()+":\n");
+            for (Map.Entry<String, String> book : ffw.getBooksInfoMap().entrySet()) {
+                System.out.println("["+counter+"] - Title: "+book.getKey()+" | Author: "+book.getValue()+"\n");
+                counter++;
+            }
+            System.out.print("");
+        }
     }
 }
